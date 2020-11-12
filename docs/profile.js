f64c7ae28de7781975a4a6c5d3f6ebd702d5dd35
Spandan Saxena <spandansaxena@gmail.com>
2020-11-12 18:33:03 +0530

    Update profile.js

--- docs/profile.js
+++ docs/profile.js
@@ -1,11 +1,2 @@
 firebase.auth().onAuthStateChanged(function(user) {
 	console.log(user);
- 	 if (user) {
- 	 	//User Signed In Do Something Like a Redirect
- 	 }
- 	  else
- 	  {
-    	//User not Signed Do Nothing
-    	//Keep this for future condtions
- 	 	}
-			});
