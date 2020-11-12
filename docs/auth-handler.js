eb0d24e1e0e9687ca830275c3c39ea4fa727c0dc
Spandan Saxena <spandansaxena@gmail.com>
2020-11-12 12:27:13 +0530

    fix #3

--- docs/auth-handler.js
+++ docs/auth-handler.js
@@ -1,10 +1,11 @@
 firebase.auth().onAuthStateChanged(function(user) {
  	 if (user) {
  	 	//User Signed In Do Something Like a Redirect
-        window.location = "https://the-rebooted-coder.github.io/Take-Notes-Web";
+        window.location = "https://the-rebooted-coder.github.io/Take-Notes-Web/take-notes";
  	 }
  	  else
  	  {
     	//Dispay an error page or reload the same page again
+          window.location = "index.html";
  	 	}
 			});
